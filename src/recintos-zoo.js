29e9368417f34a00ad0526f4a0c0953a1713e68e
Priscilla <benevenute30@gmail.com>
2024-09-07 18:40:14 -0300

    feat: validacao e filtro para animais carnivoros

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,10 +1,10 @@
 const animais = [
-    { especie: "LEAO", tamanho: 3, bioma: ["savana", "savana e rio"] },
-    { especie: "LEOPARDO", tamanho: 2, bioma: ["savana", "savana e rio"] },
-    { especie: "CROCODILO", tamanho: 3, bioma: ["rio", "savana e rio"] },
-    { especie: "MACACO", tamanho: 1, bioma: ["savana", "floresta", "savana e rio"] },
-    { especie: "GAZELA", tamanho: 2, bioma: ["savana", "savana e rio"] },
-    { especie: "HIPOPOTAMO", tamanho: 4, bioma: ["savana", "rio", "savana e rio"] },
+    { especie: "LEAO", grupoAlimentar: "carnivoro", tamanho: 3, bioma: ["savana", "savana e rio"] },
+    { especie: "LEOPARDO", grupoAlimentar: "carnivoro", tamanho: 2, bioma: ["savana", "savana e rio"] },
+    { especie: "CROCODILO", grupoAlimentar: "carnivoro", tamanho: 3, bioma: ["rio", "savana e rio"] },
+    { especie: "MACACO", grupoAlimentar: "herbivoro", tamanho: 1, bioma: ["savana", "floresta", "savana e rio"] },
+    { especie: "GAZELA", grupoAlimentar: "herbivoro", tamanho: 2, bioma: ["savana", "savana e rio"] },
+    { especie: "HIPOPOTAMO", grupoAlimentar: "herbivoro", tamanho: 4, bioma: ["savana", "rio", "savana e rio"] },
 ];
 
 const recintos = [
@@ -49,6 +49,14 @@ const recintos = [
 
 class RecintosZoo {
 
+    calcularEspacoLivre(recinto) {
+        return recinto.tamanhoTotal - recinto.animaisExistentes.reduce((total, animal) => total + animal.quantidade, 0)
+    }
+    //Funcao calcular tamanho animal vezes quantidade
+    calcularTamanhoAnimal(especie, quantidade) {
+        const informacaoAnimal = this.encontrarAnimal(especie)
+        return informacaoAnimal.tamanho * quantidade
+    }
     // Funcao para encontrar um animal dentro de array de animais
     encontrarAnimal(especie) {
         return animais.find(a => a.especie === especie)
@@ -57,18 +65,43 @@ class RecintosZoo {
     // Funcao para verificar se o recinto é adequado
     analisaRecintos(especie, quantidade) {
         const informacaoAnimal = this.encontrarAnimal(especie)
+        //Validar animal
         if (!especie) {
             return "Animal não encontrado"
-        } else if (quantidade <= 0) {
-            return "Quantidade inválida"
-        } else if (informacaoAnimal.tamanho > quantidade) {
-            return "Não há recinto viável"
-        } else {
-            const recintosViaveis = recintos.filter(r => {
-                return r.tamanhoTotal >= quantidade && r.bioma === informacaoAnimal.bioma[0]
-            })
-            return { erro: null, recintosViaveis: recintosViaveis }
         }
+        //Validar quantidade
+        if (quantidade <= 0) {
+            return "Quantidade inválida"
+        }
+        //Validar tamanho
+        // if (informacaoAnimal.tamanho > quantidade) {
+        //     return "Não há recinto viável"
+        // }
+
+        const tamanhoQuantidadeAnimal = this.calcularTamanhoAnimal(especie, quantidade)
+        if (informacaoAnimal.grupoAlimentar === "carnivoro") {
+            console.log("animal é carnivoro")
+            // TODO - vai se tornar uma funcao
+            recintos.filter(r => {
+                r.animaisExistentes.filter(e => {
+                    if (informacaoAnimal.bioma.includes(r.bioma) && e.especie === informacaoAnimal.especie) {
+                        const espacoLivre = this.calcularEspacoLivre(r)
+                        if (tamanhoQuantidadeAnimal <= espacoLivre) {
+                            console.log(`Recinto ${r.numero} (espaço livre: ${espacoLivre}, total: ${r.tamanhoTotal})`)
+                        }
+                    }
+                })
+            })
+        } else {
+            console.log("animal é herbivoro")
+            // TODO - vai se tornar uma funcao
+            recintos.filter(r => {
+            })
+        }
+
+        // return r.tamanhoTotal >= quantidade && r.bioma === informacaoAnimal.bioma[0]
+
+        // return { erro: null, recintosViaveis: recintosViaveis }
     }
 
 }
