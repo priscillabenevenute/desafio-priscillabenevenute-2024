b47377b0cbacc6ea20e166188d658f3279f47b5e
Priscilla <benevenute30@gmail.com>
2024-09-07 10:07:48 -0300

    feat: funcao encontrarAnimal

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,55 +1,71 @@
+const animais = [
+    { especie: "LEAO", tamanho: 3, bioma: ["savana", "savana e rio"] },
+    { especie: "LEOPARDO", tamanho: 2, bioma: ["savana", "savana e rio"] },
+    { especie: "CROCODILO", tamanho: 3, bioma: ["rio", "savana e rio"] },
+    { especie: "MACACO", tamanho: 1, bioma: ["savana", "floresta", "savana e rio"] },
+    { especie: "GAZELA", tamanho: 2, bioma: ["savana", "savana e rio"] },
+    { especie: "HIPOPOTAMO", tamanho: 4, bioma: ["savana", "rio", "savana e rio"] },
+];
+
+const recintos = [
+    {
+        numero: 1,
+        bioma: "savana",
+        tamanhoTotal: 10,
+        animaisExistentes: [
+            { especie: "MACACO", quantidade: 3 }
+        ]
+    },
+    {
+        numero: 2,
+        bioma: "floresta",
+        tamanhoTotal: 5,
+        animaisExistentes: []
+    },
+    {
+        numero: 3,
+        bioma: "savana e rio",
+        tamanhoTotal: 7,
+        animaisExistentes: [
+            { especie: "GAZELA", quantidade: 1 }
+        ]
+    },
+    {
+        numero: 4,
+        bioma: "rio",
+        tamanhoTotal: 8,
+        animaisExistentes: []
+    },
+    {
+        numero: 5,
+        bioma: "savana",
+        tamanhoTotal: 9,
+        animaisExistentes: [
+            { especie: "LEAO", quantidade: 1 }
+        ]
+    }
+];
+
+
 class RecintosZoo {
 
     analisaRecintos(animal, quantidade) {
-        const animais = [
-            { especie: "LEAO", tamanho: 3, bioma: ["savana", "savana e rio"] },
-            { especie: "LEOPARDO", tamanho: 2, bioma: ["savana", "savana e rio"] },
-            { especie: "CROCODILO", tamanho: 3, bioma: ["rio", "savana e rio"] },
-            { especie: "MACACO", tamanho: 1, bioma: ["savana", "floresta", "savana e rio"] },
-            { especie: "GAZELA", tamanho: 2, bioma: ["savana", "savana e rio"] },
-            { especie: "HIPOPOTAMO", tamanho: 4, bioma: ["savana", "rio", "savana e rio"] },
-        ];
-
-        const recintos = [
-            {
-                numero: 1,
-                bioma: "savana",
-                tamanhoTotal: 10,
-                animaisExistentes: [
-                    { especie: "MACACO", quantidade: 3 }
-                ]
-            },
-            {
-                numero: 2,
-                bioma: "floresta",
-                tamanhoTotal: 5,
-                animaisExistentes: []
-            },
-            {
-                numero: 3,
-                bioma: "savana e rio",
-                tamanhoTotal: 7,
-                animaisExistentes: [
-                    { especie: "GAZELA", quantidade: 1 }
-                ]
-            },
-            {
-                numero: 4,
-                bioma: "rio",
-                tamanhoTotal: 8,
-                animaisExistentes: []
-            },
-            {
-                numero: 5,
-                bioma: "savana",
-                tamanhoTotal: 9,
-                animaisExistentes: [
-                    { especie: "LEAO", quantidade: 1 }
-                ]
-            }
-        ]
+    }
+    // Funcao para encontrar um animal dentro de array de animais
+    encontrarAnimal(especie) {
+        console.log(animais.find(a => a.especie === especie))
     }
 
+    //Funcao para verificar se o recinto é o adequado
+    verificaRecinto(recinto, especie, quantidade) {
+        const informacaoAnimal = encontrarAnimal(especie)
+        if (!encontrarAnimal) {
+            return "Animal não encontrado"
+        }
+    }
+
+    //Funcao para verificar se o recinto tem espaco
 }
 
 export { RecintosZoo as RecintosZoo };
+const resultado = new RecintosZoo().encontrarAnimal('MACACO');
--- src/recintos-zoo.test.js
+++ src/recintos-zoo.test.js
@@ -3,22 +3,22 @@ import { RecintosZoo } from "./recintos-zoo.js";
 describe('Recintos do Zoologico', () => {
 
     test('Deve rejeitar animal inválido', () => {
-            const resultado = new RecintosZoo().analisaRecintos('UNICORNIO', 1);
-            expect(resultado.erro).toBe("Animal inválido");
-            expect(resultado.recintosViaveis).toBeFalsy();
-        });
+        const resultado = new RecintosZoo().analisaRecintos('UNICORNIO', 1);
+        expect(resultado.erro).toBe("Animal inválido");
+        expect(resultado.recintosViaveis).toBeFalsy();
+    });
 
     test('Deve rejeitar quantidade inválida', () => {
-            const resultado = new RecintosZoo().analisaRecintos('MACACO', 0);
-            expect(resultado.erro).toBe("Quantidade inválida");
-            expect(resultado.recintosViaveis).toBeFalsy();
+        const resultado = new RecintosZoo().analisaRecintos('MACACO', 0);
+        expect(resultado.erro).toBe("Quantidade inválida");
+        expect(resultado.recintosViaveis).toBeFalsy();
     });
 
     test('Não deve encontrar recintos para 10 macacos', () => {
-            const resultado = new RecintosZoo().analisaRecintos('MACACO', 10);
-            expect(resultado.erro).toBe("Não há recinto viável");
-            expect(resultado.recintosViaveis).toBeFalsy();
-        });
+        const resultado = new RecintosZoo().analisaRecintos('MACACO', 10);
+        expect(resultado.erro).toBe("Não há recinto viável");
+        expect(resultado.recintosViaveis).toBeFalsy();
+    });
 
     test('Deve encontrar recinto para 1 crocodilo', () => {
         const resultado = new RecintosZoo().analisaRecintos('CROCODILO', 1);
