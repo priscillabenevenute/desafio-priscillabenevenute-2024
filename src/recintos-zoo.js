b850fa4de84bdd2232ec2589c6311831cee730dd
Priscilla <benevenute30@gmail.com>
2024-09-09 23:00:38 -0300

    feat: validando filtro para animais herbivoros

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -29,7 +29,8 @@ const recintos = [
         bioma: "savana e rio",
         tamanhoTotal: 7,
         animaisExistentes: [
-            { especie: "GAZELA", quantidade: 1 }
+            { especie: "GAZELA", quantidade: 1 },
+            { especie: "MACACO", quantidade: 1 }
         ]
     },
     {
@@ -42,8 +43,7 @@ const recintos = [
         numero: 5,
         bioma: "savana",
         tamanhoTotal: 9,
-        animaisExistentes: [
-        ]
+        animaisExistentes: []
     }
 ];
 
@@ -51,7 +51,8 @@ const recintos = [
 class RecintosZoo {
 
     calcularEspacoLivre(recinto) {
-        return recinto.tamanhoTotal - recinto.animaisExistentes.reduce((total, animal) => total + animal.quantidade, 0)
+        const result = recinto.tamanhoTotal - recinto.animaisExistentes.reduce((total, animal) => total + animal.quantidade, 0)
+        return result;
     }
     //Funcao calcular tamanho animal vezes quantidade
     calcularTamanhoAnimal(especie, quantidade) {
@@ -106,11 +107,80 @@ class RecintosZoo {
                     }
                 }
             })
+            // TODO - retornar array com recintos viaveis aqui
         } else {
             console.log("animal é herbivoro")
             // TODO - vai se tornar uma funcao
             recintos.filter(r => {
+                if (r.animaisExistentes.length !== 0) {
+                    r.animaisExistentes.filter(e => {
+                        if (e.especie === "HIPOPOTAMO") {
+                            if (r.bioma === "savana") {
+                                if (informacaoAnimal.especie === e.especie) {
+                                    const espacoLivre = this.calcularEspacoLivre(r)
+                                    if (tamanhoQuantidadeAnimal <= espacoLivre) {
+                                        console.log(`Recinto ${r.numero} (espaço livre: ${espacoLivre}, total: ${r.tamanhoTotal})`)
+                                    } else {
+                                        console.log("NAO CABE")
+                                    }
+                                } else {
+                                    console.log("Hipopotamo nao aceita outro animal na savana")
+                                }
+                            } else if (r.bioma !== "savana") {
+                                if (informacaoAnimal.especie === e.especie) {
+                                    const espacoLivre = this.calcularEspacoLivre(r)
+                                    if (tamanhoQuantidadeAnimal <= espacoLivre) {
+                                        console.log(`Recinto ${r.numero} (espaço livre: ${espacoLivre}, total: ${r.tamanhoTotal})`)
+                                    } else {
+                                        console.log("NAO CABE")
+                                    }
+                                } else {
+                                    const espacoLivre = this.calcularEspacoLivre(r)
+                                    if (tamanhoQuantidadeAnimal + 1 <= espacoLivre) {
+                                        console.log(`Recinto ${r.numero} (espaço livre: ${espacoLivre}, total: ${r.tamanhoTotal})`)
+                                    } else {
+                                        console.log("NAO CABE")
+                                    }
+                                }
+                            }
+                        }
+                        // Se nao for hipopotamo
+                        else {
+                            if (informacaoAnimal.bioma.includes(r.bioma) && e.especie === informacaoAnimal.especie) {
+                                const espacoLivre = this.calcularEspacoLivre(r)
+                                if (tamanhoQuantidadeAnimal <= espacoLivre) {
+                                    console.log(`Recinto ${r.numero} (espaço livre: ${espacoLivre}, total: ${r.tamanhoTotal})`)
+                                } else {
+                                    console.log("NAO CABE")
+                                }
+                            } else if (r.bioma !== "savana" && informacaoAnimal.bioma.includes(r.bioma) && e.especie !== informacaoAnimal.especie) {
+                                const espacoLivre = this.calcularEspacoLivre(r)
+                                if (tamanhoQuantidadeAnimal + 1 <= espacoLivre) {
+                                    console.log(`Recinto ${r.numero} (espaço livre: ${espacoLivre}, total: ${r.tamanhoTotal})`)
+                                }
+                            } else {
+                                console.log("Recinto nao viavel")
+                            }
+                        }
+                    })
+                } else {
+                    if (informacaoAnimal.especie === "MACACO" && quantidade < 2) {
+                        console.log("Recinto nao viavel")
+                    } else {
+                        if (informacaoAnimal.bioma.includes(r.bioma)) {
+                            const espacoLivre = this.calcularEspacoLivre(r)
+                            if (tamanhoQuantidadeAnimal + 1 <= espacoLivre) {
+                                console.log(`Recinto ${r.numero} (espaço livre: ${espacoLivre}, total: ${r.tamanhoTotal})`)
+                            } else {
+                                console.log("NAO CABE")
+                            }
+                        } else {
+                            console.log("Recinto nao viavel")
+                        }
+                    }
+                }
             })
+            // TODO - retornar array com recintos viaveis aqui
         }
 
         // return r.tamanhoTotal >= quantidade && r.bioma === informacaoAnimal.bioma[0]
@@ -120,4 +190,4 @@ class RecintosZoo {
 
 }
 export { RecintosZoo as RecintosZoo };
-const resultado = new RecintosZoo().analisaRecintos('LEAO', 2);
+const resultado = new RecintosZoo().analisaRecintos('HIPOPOTAMO', 1);
